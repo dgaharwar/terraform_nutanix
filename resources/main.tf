ff8b9b85c6ae4f625b381e12903bbe0f965ec25f
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-10-08 22:14:20 +0100

    Update main.tf

--- resources/main.tf
+++ resources/main.tf
@@ -24,14 +24,14 @@ provider "nutanix" {
 }
 
 #resource "nutanix_image" "image" {
-#  name        = "Windows Server 2019"
-#  description = "17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
+#  name        = "Arch Linux"
+#  description = "Arch-Linux-x86_64-basic-20241001.267073.qcow2"
 #  source_uri  = "https://mirror.pkgbuild.com/images/v20241001.267073/Arch-Linux-x86_64-basic-20241001.267073.qcow2"
 #}
 
 resource "nutanix_image" "image" {
-  name         = "https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"
-  description  = "https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"
+  name         = "Windows Server 2019"
+  description  = "17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
   source_url   = "https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"
 }
 
